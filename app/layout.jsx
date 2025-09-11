81999b3c9d24e8b41f7c6367214ec7e1949674e3
Vatsal Solanki <vatsal.solanki@gmail.com>
2025-09-11 11:16:31 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -92,7 +92,7 @@ export default function RootLayout({ children }) {
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "t858lerf5g");
+              })(window, document, "clarity", "script", "t8xl6gfz2s");
             `,
           }}
         />
